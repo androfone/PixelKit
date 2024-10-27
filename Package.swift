2ab60bf51268f6392ed20f2e893dff393bdc8798
AndroPhone <gshababa321@gmail.com>
2024-10-27 11:01:34 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "MyLibrary",
+            name: "PixelKit",
             targets: ["MyLibrary"]),
     ],
     dependencies: [
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "MyLibrary",
+            name: "PixelKit",
             dependencies: []),
         .testTarget(
             name: "MyLibraryTests",
